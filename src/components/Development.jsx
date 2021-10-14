6cf488ac3845b6f87caaae060f4763be97b4010f
Ananda Zulham <anandahasibuan13@gmail.com>
2021-10-14 08:14:06 +0700

    custom color link

--- src/components/Development.jsx
+++ src/components/Development.jsx
@@ -14,19 +14,19 @@ export default function Development() {
 		<p>but most developers are no longer active Facebook users.</p>
 		<h4 className="pt-3">Reporting Bugs</h4>
 		<hr />
-		<p>For bugreports please go to <a href="https://sourceforge.net/p/bluefish/tickets/" target="_blank" rel="noreferrer">our bugtracker.</a></p>
+		<p>For bugreports please go to <a href="https://sourceforge.net/p/bluefish/tickets/" target="_blank" rel="noreferrer" className="text-danger">our bugtracker.</a></p>
 		<h4 className="pt-3">Mailing lists</h4>
 		<hr />
-		<p>switched to a new mailinglist to discuss development, features, bugs, ideas, etc. You can subscribe at <a href="https://lists.sourceforge.net/lists/listinfo/bluefish-community" target="_blank" rel="noreferrer">https://lists.sourceforge.net/lists/listinfo/bluefish-community</a>. There are also <a href="https://sourceforge.net/p/bluefish/mailman/bluefish-community/" target="_blank" rel="noreferrer">archives for the community list</a>.
+		<p>switched to a new mailinglist to discuss development, features, bugs, ideas, etc. You can subscribe at <a href="https://lists.sourceforge.net/lists/listinfo/bluefish-community" target="_blank" rel="noreferrer" className="text-danger">https://lists.sourceforge.net/lists/listinfo/bluefish-community</a>. There are also <a href="https://sourceforge.net/p/bluefish/mailman/bluefish-community/" target="_blank" rel="noreferrer" className="text-danger">archives for the community list</a>.
 
 		  Our old mailing lists can also be read as newsgroups at gmane bluefish-dev and gmane bluefish-users.</p>
 		<h4 className="pt-3">Help needed</h4>
 		<hr />
 		<p>Help is welcome on various parts:</p>
 		<ul className="ml-n3">
-		  <li className="mb-2">We are working on <span className="font-weight-bold">more <a href="https://bfwiki.tellefsen.net/index.php/DevelopingPlugins">plugins</a></span>, which requires some C, gtk or python knowledge</li>
+		  <li className="mb-2">We are working on <span className="font-weight-bold">more <a href="https://bfwiki.tellefsen.net/index.php/DevelopingPlugins" className="text-danger">plugins</a></span>, which requires some C, gtk or python knowledge</li>
 		  <li className="mb-2">Writing the <span className="font-weight-bold">manual</span> in our wiki</li>
-		  <li className="mb-2"><span className="font-weight-bold">Translations</span> - several languages are out of date, see the <a href="http://bfwiki.tellefsen.net/index.php/Translations">WiKi</a></li>
+		  <li className="mb-2"><span className="font-weight-bold">Translations</span> - several languages are out of date, see the <a href="http://bfwiki.tellefsen.net/index.php/Translations" className="text-danger">WiKi</a></li>
 		  <li className="mb-2">Testing</li>
 		  <li className="mb-2">Creating packages - we currently lack a <span className="font-weight-bold">Mandrake</span> and a <span className="font-weight-bold">Suse</span> maintainer!</li>
 		  <li className="mb-2">Better native Windows support, there are many bugs specific to windows reported!</li>
@@ -34,7 +34,7 @@ export default function Development() {
 		</ul>
 		<h4 className="pt-3">Code in Subversion</h4>
 		<hr />
-		<p>Bluefish uses SVN (Subversion) hosted by  <a href="http://sourceforge.net/projects/bluefish/">Sourceforge</a> to manage the code. You can <a href="https://sourceforge.net/p/bluefish/code/HEAD/tree/trunk/bluefish/">browse the repository</a> on-line.
+		<p>Bluefish uses SVN (Subversion) hosted by  <a href="http://sourceforge.net/projects/bluefish/" className="text-danger">Sourceforge</a> to manage the code. You can <a href="https://sourceforge.net/p/bluefish/code/HEAD/tree/trunk/bluefish/" className="text-danger">browse the repository</a> on-line.
 
 		To build the latest Bluefish version from SVN:</p>
 		<div className="bg-light px-sm-2">
@@ -45,12 +45,12 @@ export default function Development() {
 		  <p className="text-monospace mb-1">make</p>
 		  <p className="text-monospace mb-1">su -c "make install"</p>
 		</div>
-		<p className="mt-3">For more information see the wiki page <a href="http://bfwiki.tellefsen.net/index.php/Compiling_Bluefish_from_source">Compiling Bluefish from source code</a>.</p>
+		<p className="mt-3">For more information see the wiki page <a href="http://bfwiki.tellefsen.net/index.php/Compiling_Bluefish_from_source" className="text-danger">Compiling Bluefish from source code</a>.</p>
 		<h4 className="pt-3">Bleeding edge Debian/Ubuntu packages</h4>
 		<hr />
-		<p>There is a Bluefish-snapshots PPA at <a href="https://launchpad.net/~klaus-vormweg/+archive/ubuntu/bluefish-snapshots">https://launchpad.net/~klaus-vormweg/+archive/ubuntu/bluefish-snapshots</a>. Use these at your own risk, the software in these packages is still under development!</p>
+		<p>There is a Bluefish-snapshots PPA at <a href="https://launchpad.net/~klaus-vormweg/+archive/ubuntu/bluefish-snapshots" className="text-danger">https://launchpad.net/~klaus-vormweg/+archive/ubuntu/bluefish-snapshots</a>. Use these at your own risk, the software in these packages is still under development!</p>
 
-		<p>Debian/Ubuntu packages can also be created from the subversion repository.  <span className="font-weight-bold">These are NOT official packages and should only be distributed as "bluefish-unstable". Only use the script and the created packages if you follow the development. For "normal" users, official packages are <Link href="/downloads" className="text-primary">available here</Link></span>.</p>
+		<p>Debian/Ubuntu packages can also be created from the subversion repository.  <span className="font-weight-bold">These are NOT official packages and should only be distributed as "bluefish-unstable". Only use the script and the created packages if you follow the development. For "normal" users, official packages are <Link href="/downloads" className="text-danger">available here</Link></span>.</p>
 		<div className="bg-light px-sm-2">
 		  <p className="text-monospace mb-1 text-break">svn co svn://svn.code.sf.net/p/bluefish/code/trunk/bluefish</p>
 		  <p className="text-monospace mb-1">cd bluefish</p>
@@ -60,12 +60,12 @@ export default function Development() {
 		<hr />
 		<p>Olivier Sessink is also known for:</p>
 		<ul className="ml-n3">
-		  <li className="mb-2"><a href="http://olivier.sessink.nl/directoryassistant/">Directory Assistant</a> - an LDAP addressbook frontend</li>
-		  <li className="mb-2"><a href="https://olivier.sessink.nl/jailkit/">Jailkit</a> - a set of utilities to limit user accounts</li>
-		  <li className="mb-2"><a href="https://olivier.sessink.nl/scannedonly/">Scannedonly</a> - a scalable high performance Samba anti virus module</li>
-		  <li className="mb-2"><a href="http://rro.sourceforge.net/">Risk Reduction Overview</a> or Risico Reductie Overzicht - a risk management tool</li>
+		  <li className="mb-2"><a href="http://olivier.sessink.nl/directoryassistant/" className="text-danger">Directory Assistant</a> - an LDAP addressbook frontend</li>
+		  <li className="mb-2"><a href="https://olivier.sessink.nl/jailkit/" className="text-danger">Jailkit</a> - a set of utilities to limit user accounts</li>
+		  <li className="mb-2"><a href="https://olivier.sessink.nl/scannedonly/" className="text-danger">Scannedonly</a> - a scalable high performance Samba anti virus module</li>
+		  <li className="mb-2"><a href="http://rro.sourceforge.net/" className="text-danger">Risk Reduction Overview</a> or Risico Reductie Overzicht - a risk management tool</li>
 		  <p>Daniel Leidert is also known for:</p>
-		  <li className="mb-2"><a href="https://qa.debian.org/developer.php?login=daniel.leidert@wgdd.de">lots of Debian packages</a>, and <a href="https://bugs.launchpad.net/~dleidert">Ubuntu packages</a></li>
+		  <li className="mb-2"><a href="https://qa.debian.org/developer.php?login=daniel.leidert@wgdd.de" className="text-danger">lots of Debian packages</a>, and <a href="https://bugs.launchpad.net/~dleidert" className="text-danger">Ubuntu packages</a></li>
 		</ul>
 	  </div>
 	</>
--- src/components/Downloads.jsx
+++ src/components/Downloads.jsx
@@ -7,26 +7,26 @@ export default function Downloads() {
 		<h3>Official Bluefish releases and documentation</h3>
 		<hr />
 		<p>The latest stable version is Bluefish 2.2.12 which can be downloaded from our download server in Annaberg-Buchholz, Germany: </p>
-		<a href="http://www.bennewitz.com/bluefish/stable/source/" className="btn btn-primary btn-sm" target="_blank" rel="noreferrer">Source Code</a>
+		<a href="http://www.bennewitz.com/bluefish/stable/source/" className="btn btn-primary btn-sm text-light" target="_blank" rel="noreferrer">Source Code</a>
 		<p className="mt-3">Availability of binary packages might take a short while:</p>
 		<ul className="list-unstyled d-flex flex-wrap align-items-center">
-		  <li><a href="http://www.bennewitz.com/bluefish/stable/binaries/fedora/" className="btn btn-primary btn-sm mr-2" target="_blank" rel="noreferrer">Fedora Linux Packages</a></li>
-		  <li><a href="http://www.bennewitz.com/bluefish/stable/binaries/macosx/" className="btn btn-primary btn-sm my-2 mr-2" target="_blank" rel="noreferrer">OSX Installer</a></li>
-		  <li><a href="http://www.bennewitz.com/bluefish/stable/binaries/win32/" target="_blank" rel="noreferrer" className="btn btn-primary btn-sm">Win32 Installer</a></li>
+		  <li><a href="http://www.bennewitz.com/bluefish/stable/binaries/fedora/" className="btn btn-primary btn-sm mr-2 text-light" target="_blank" rel="noreferrer">Fedora Linux Packages</a></li>
+		  <li><a href="http://www.bennewitz.com/bluefish/stable/binaries/macosx/" className="btn btn-primary btn-sm my-2 mr-2 text-light" target="_blank" rel="noreferrer">OSX Installer</a></li>
+		  <li><a href="http://www.bennewitz.com/bluefish/stable/binaries/win32/" target="_blank" rel="noreferrer" className="btn btn-primary btn-sm text-light">Win32 Installer</a></li>
 		</ul>
 		<p>Alternatively some files are also available through Sourceforge:</p>
-		<a href="https://sourceforge.net/projects/bluefish/files/" className="btn btn-primary btn-sm" target="_blank" rel="noreferrer">Sourceforge.net</a>
+		<a href="https://sourceforge.net/projects/bluefish/files/" className="btn btn-primary btn-sm text-light" target="_blank" rel="noreferrer">Sourceforge.net</a>
 		<h4 className="mt-3">Installers and packages</h4>
-		<p>The Bluefish wiki has a page <a href="https://bfwiki.tellefsen.net/index.php/Installing_Bluefish">how to install Bluefish</a> on various different operating systems and distributions, such as Debian, Ubuntu, Fedora, Gentoo, Mac OSX, AltLinux, OpenSolaris and Windows.</p>
+		<p>The Bluefish wiki has a page <a href="https://bfwiki.tellefsen.net/index.php/Installing_Bluefish" className="text-danger">how to install Bluefish</a> on various different operating systems and distributions, such as Debian, Ubuntu, Fedora, Gentoo, Mac OSX, AltLinux, OpenSolaris and Windows.</p>
 		<ul className="list-unstyled d-flex flex-wrap align-items-center">
-		  <li><a href="http://bfwiki.tellefsen.net/index.php/Installing_Bluefish#Installing_the_very_latest_on_Ubuntu_Linux" target="_blank" rel="noreferrer" className="btn btn-primary btn-sm mb-2 mr-2">Ubuntu Linux</a></li>
-		  <li><a href="http://bfwiki.tellefsen.net/index.php/Installing_Bluefish#Installing_the_very_latest_release_on_Debian" target="_blank" rel="noreferrer" className="btn btn-primary btn-sm mb-2 mr-2">Debian Linux</a></li>
-		  <li><a href="http://bfwiki.tellefsen.net/index.php/Installing_Bluefish#Installing_the_very_latest_on_Fedora_with_yum" target="_blank" rel="noreferrer" className="btn btn-primary btn-sm mb-2 mr-2">Fedora Linux</a></li>
-		  <li><a href="http://bfwiki.tellefsen.net/index.php/Installing_Bluefish#Installing_Bluefish_on_Mac_OS_X" target="_blank" rel="noreferrer" className="btn btn-primary btn-sm mb-2 mr-2">OSX</a></li>
-		  <li><a href="http://bfwiki.tellefsen.net/index.php/Installing_Bluefish#Installing_Bluefish_on_Windows_XP_or_newer" target="_blank" rel="noreferrer" className="btn btn-primary btn-sm mb-2 mr-2">Windows</a></li>
+		  <li><a href="http://bfwiki.tellefsen.net/index.php/Installing_Bluefish#Installing_the_very_latest_on_Ubuntu_Linux" target="_blank" rel="noreferrer" className="btn btn-primary btn-sm mb-2 mr-2 text-light">Ubuntu Linux</a></li>
+		  <li><a href="http://bfwiki.tellefsen.net/index.php/Installing_Bluefish#Installing_the_very_latest_release_on_Debian" target="_blank" rel="noreferrer" className="btn btn-primary btn-sm mb-2 mr-2 text-light">Debian Linux</a></li>
+		  <li><a href="http://bfwiki.tellefsen.net/index.php/Installing_Bluefish#Installing_the_very_latest_on_Fedora_with_yum" target="_blank" rel="noreferrer" className="btn btn-primary btn-sm mb-2 mr-2 text-light">Fedora Linux</a></li>
+		  <li><a href="http://bfwiki.tellefsen.net/index.php/Installing_Bluefish#Installing_Bluefish_on_Mac_OS_X" target="_blank" rel="noreferrer" className="btn btn-primary btn-sm mb-2 mr-2 text-light">OSX</a></li>
+		  <li><a href="http://bfwiki.tellefsen.net/index.php/Installing_Bluefish#Installing_Bluefish_on_Windows_XP_or_newer" target="_blank" rel="noreferrer" className="btn btn-primary btn-sm mb-2 mr-2 text-light">Windows</a></li>
 		</ul>
 		<h4 className="pt-3">Install From Source Code</h4>
-		<p>The 2.2.12 release requires <a href="https://www.gtk.org/" target="_blank" rel="noreferrer">gtk-2 (gtk 2.20 or newer) or gtk-3 (preferably 3.4 or newer)</a> and optionally <a href="http://www.abisource.com/projects/enchant/" target="_blank" rel="noreferrer">libenchant</a> for spell checking, <a href="http://live.gnome.org/Gucharmap" target="_blank" rel="noreferrer">libgucharmap</a> for the character map plugin, and <a href="https://www.python.org/" target="_blank" rel="noreferrer">python</a> version 2 or 3 for the <a href="https://code.google.com/archive/p/zen-coding/" target="_blank" rel="noreferrer">zencoding</a> plugin and integrated tools like jsmin, and cssmin.</p>
+		<p>The 2.2.12 release requires <a href="https://www.gtk.org/" target="_blank" rel="noreferrer" className="text-danger">gtk-2 (gtk 2.20 or newer) or gtk-3 (preferably 3.4 or newer)</a> and optionally <a href="http://www.abisource.com/projects/enchant/" target="_blank" rel="noreferrer" className="text-danger">libenchant</a> for spell checking, <a href="http://live.gnome.org/Gucharmap" target="_blank" rel="noreferrer" className="text-danger">libgucharmap</a> for the character map plugin, and <a href="https://www.python.org/" target="_blank" rel="noreferrer" className="text-danger">python</a> version 2 or 3 for the <a href="https://code.google.com/archive/p/zen-coding/" target="_blank" rel="noreferrer" className="text-danger">zencoding</a> plugin and integrated tools like jsmin, and cssmin.</p>
 		<h4 className="pt-3">Check your download</h4>
 		<p>If you want to make that the file you've downloaded is not corrupted check the SHA-1 checksums:</p>
 		<div className="bg-light px-sm-2">
@@ -54,8 +54,8 @@ export default function Downloads() {
 		<hr />
 		<p>There are several different sets of contributed data. If you have anything please send it to the bluefish team!</p>
 		<ul className="list-unstyled">
-		  <Link href="/icons">'made with Bluefish' icons</Link>
-		  <li><a href="https://bfwiki.tellefsen.net/index.php/Main_Page">contributed data in the wiki, a couple of language definition files</a></li>
+		  <Link href="/icons" className="text-danger">'made with Bluefish' icons</Link>
+		  <li><a href="https://bfwiki.tellefsen.net/index.php/Main_Page" className="text-danger">contributed data in the wiki, a couple of language definition files</a></li>
 		</ul>
 	  </div>
 	</>
